34d5aad97b70c6893b5d2fcd4f48572088bbca6c
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-03-01 15:35:29 +0000

    Updated bike, docking_station and docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,21 +2,20 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:bike) {DockingStation.new.release_bike}
+
   it {is_expected.to respond_to(:release_bike)}
 
   it 'releases a bike' do
-    station = DockingStation.new
-    bike = station.release_bike
     expect(bike).to be_a Bike
   end
 
-end
-
-describe 'Released bike' do
-
-  it 'has method working?' do
-    bike = DockingStation.new.release_bike
+  it 'can test bike works' do
     expect(bike).to respond_to :working?
+  end
+
+  it 'bike works' do
+    expect(bike).to be_working
   end
 
 end
