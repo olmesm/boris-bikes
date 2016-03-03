8997d6a8461819cb2a7839ca9cafd1d0558d0dac
Murilo Dal Ri <murilodalri@Murilos-MacBook-Pro.local>
2016-03-03 17:03:27 +0000

    Complete van collection method

--- lib/van.rb
+++ lib/van.rb
@@ -1,4 +1,5 @@
 class Van
+  attr_accessor :loaded_bikes
   def collect_broken_bikes(docking_station)
     @loaded_bikes = docking_station.bikes.reject(&:working?)
     docking_station.bikes -= @loaded_bikes
